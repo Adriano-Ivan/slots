b4236b0dc1d0279ef0b2b71f46673c4849cb404b
Adriano-Ivan <adrianoivandepaulacampos@gmail.com.br>
2022-03-06 12:59:23 -0300

    Utilizando slots em formulário.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <help-form>
+    <template v-slot:help>
+      <p>{{ help_text }}</p>
+    </template>
+    <template v-slot:fields>
+      <input type="text" placeholder="email" />
+      <input type="text" placeholder="username" />
+      <input type="password" password="password" />
+    </template>
+    <template v-slot:buttons>
+      <button type="submit">Submit</button>
+    </template>
+    <p>Some text</p>
+  </help-form>
+  <help-form>
+    <template v-slot:help>
+      <p>Contact help text</p>
+    </template>
+    <template v-slot:fields>
+      <input type="text" placeholder="name" />
+      <input type="text" placeholder="message" />
+    </template>
+    <template v-slot:buttons>
+      <button type="submit">Submit</button>
+    </template>
+  </help-form>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelpForm from "./components/HelpFormComponent.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelpForm,
+  },
+  data() {
+    return {
+      help_text: "This is some help text.",
+    };
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
